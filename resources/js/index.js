8f6861b815165118b2564a6b3d93e7edf0518f40
delanc3 <joshtrueman219@gmail.com>
2023-05-06 17:10:45 +0100

    added 'about me' page;

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,26 @@
 requirejs(['jquery', 'mousetrap.min', 'cookies.min'], function ($, Mousetrap, Cookies) {
-	console.log("Script Connected");
+	var isScrollEventRegistered = false;
+
+	$(window).bind('mousewheel', function(event) {
+		if (event.originalEvent.wheelDelta >= 0) {
+			if (isScrollEventRegistered) {
+				console.log('scroll up');
+				$('#border').removeClass('transition');
+				$('#titleCard').removeClass('transition');
+				$('#aboutMe').removeClass('transition');
+				isScrollEventRegistered = false;
+			}
+		} else {
+			if (!isScrollEventRegistered) {
+				console.log('scroll down');
+				$('#border').addClass('transition');
+				$('#titleCard').addClass('transition');
+				setTimeout(function() {
+					$('#aboutMe').addClass('transition');
+				}, 400);
+				isScrollEventRegistered = true;
+			}
+		}
+	});
+
 });
